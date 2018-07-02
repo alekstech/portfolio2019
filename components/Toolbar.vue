7507d53eb04a434769d48cb1ea4450dbe1927add
Aleks <z.sobieraj.spam@gmail.com>
2018-07-02 12:58:10 -0400

    Toolbar

---
+++ components/Toolbar.vue
@@ -0,0 +1,14 @@
+<v-toolbar flat>
+
+    <v-toolbar-title>Aleks Sobieraj</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-sm-and-down">
+
+        <v-btn flat>Blog</v-btn>
+        <v-btn flat>Portfolio</v-btn>
+
+    </v-toolbar-items>
+
+</v-toolbar>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <navbar/>
+    <Toolbar/>
     <nuxt/>
   </div>
 </template>
