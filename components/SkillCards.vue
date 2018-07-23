bf25da461da8a6792540389632beb61b356afce0
Aleks <z.sobieraj.spam@gmail.com>
2018-07-22 21:51:17 -0400

    technology logos

--- components/SkillCards.vue
+++ components/SkillCards.vue
@@ -1,7 +1,48 @@
 <template>
   <v-container>
-    <v-flex>
+    <v-flex
+      d-inline-flex
+      xs1
+      class="tech-logo">
       <vue-logo />
+    </v-flex>
+    <v-flex
+      d-inline-flex
+      xs1
+      class="tech-logo">
+      <git-logo />
+    </v-flex>
+    <v-flex
+      d-inline-flex
+      xs1
+      class="tech-logo">
+      <webpack-logo />
+    </v-flex>
+    <v-flex
+      d-inline-flex
+      xs1
+      class="tech-logo">
+      <vuetify-logo />
+    </v-flex>
+    <v-flex
+      d-inline-flex
+      xs1
+      class="tech-logo">
+      <nuxt-logo />
+    </v-flex>
+    <v-flex
+      d-inline-flex
+      xs1
+      class="tech-logo">
+      <js-logo />
+    </v-flex>
+    <v-flex
+      d-inline-flex
+      xs1
+      class="tech-logo">
+      <css-logo />
+    </v-flex>
+    <v-flex>
       <p>I built</p>
       <ul>
         <li>brochure sites, to match provided designs</li>
@@ -39,11 +80,33 @@
   </v-container>
 </template>
 <script>
-import VueLogo from '~/assets/images/Vue.js_Logo.svg'
+import VueLogo from '~/assets/images/VueLogo.svg'
+import GitLogo from '~/assets/images/GitLogo.svg'
+import JSLogo from '~/assets/images/JSLogo.svg'
+import CSSLogo from '~/assets/images/CSSLogo.svg'
+import WebpackLogo from '~/assets/images/WebpackLogo.svg'
+import NuxtLogo from '~/assets/images/NuxtLogo.svg'
+import VuetifyLogo from '~/assets/images/VuetifyLogo.svg'
+
 export default {
   name: 'Skill',
   components: {
-    'vue-logo': VueLogo
+    'vue-logo': VueLogo,
+    'git-logo': GitLogo,
+    'js-logo': JSLogo,
+    'css-logo': CSSLogo,
+    'webpack-logo': WebpackLogo,
+    'nuxt-logo': NuxtLogo,
+    'vuetify-logo': VuetifyLogo
   }
 }
 </script>
+
+<style>
+.tech-logo {
+  height: 10vh;
+  min-height: 50px;
+  padding: 10px
+}
+</style>
+
