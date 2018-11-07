e699e33e24a8b93d1b4ce7a3fb5a2e81dd1a12fb
Aleks Sobieraj <aleks@unoapp.com>
2018-11-07 00:15:56 -0500

    Error message

--- components/Contact.vue
+++ components/Contact.vue
@@ -50,7 +50,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .display-flex {
   display: flex;
 }
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,23 @@
 <template>
-  <p>ERROR: if this page were a redirect to default layout, errors could be handled using the snackbar</p>
+  <div class="height-100 width-100 display-flex justify-content-center align-items-center">
+    <p>This page was intentionally left blank</p>
+  </div>
 </template>
+
+<style>
+.height-100 {
+  height: 100%;
+}
+.width-100 {
+  width: 100%;
+}
+.display-flex {
+  display: flex;
+}
+.justify-content-center {
+  justify-content: center;
+}
+.align-items-center {
+  align-items: center;
+}
+</style>
