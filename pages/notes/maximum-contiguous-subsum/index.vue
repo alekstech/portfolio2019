c68cdd21cf5297ad4f43b98665777f449feb1a37
Aleks <contact@aleks.tech>
2018-11-21 21:31:36 -0500

    pre and code tags

--- pages/notes/maximum-contiguous-subsum/index.vue
+++ pages/notes/maximum-contiguous-subsum/index.vue
@@ -1,30 +1,36 @@
 <template>
-  <div>
-    <p>Given an array of integers, find the largest contiguous subsum (i.e. the largest of the sums of sections of the array read left to right where the right element is larger than the left)</p>
-    <pre>
-      const arr = [1, 2, 9, 0, 8]
+  <main class="min-h-full flex flex-col items-center py-8 lg:py-12 px-4 lg:px-8 bg-indigo-lightest">
+    <div class="flex flex-col max-w-sm">
+      <p>Given an array of integers, find the largest contiguous subsum (i.e. the largest of the sums of sections of the array read left to right where the right element is larger than the left)</p>
+      <pre>
+      <code>
+const arr = [1, 2, 9, 0, 8]
 
-      function findMaximumContiguousSubsum (arr) {
-        let max = arr[0];
+function findMaximumContiguousSubsum (arr) {
 
-        let i = 0;
-        for (i; i less than arr.length; i++) {
-          let subsum = arr[i];
+  let max = arr[0];
+  let i = 0;
 
+  for (i; i less than arr.length; i++) {
+    let subsum = arr[i];
 
-          for (let j = i; j less than arr.length; j++) {
-            if (arr[j] less than arr[j + 1]) {
-              subsum += arr[j + 1]
-            } else break
-          }
-          if (subsum > max) {
-            max = subsum
-          }
-        }
-        return max
-      }
+    for (let j = i; j less than arr.length; j++) {
+      if (arr[j] less than arr[j + 1]) {
+        subsum += arr[j + 1]
+      } else break
+    }
 
-      findMaximumContiguousSubsum(arr)
-    </pre>
-  </div>
+    if (subsum > max) {
+      max = subsum
+    }
+  }
+
+  return max
+}
+
+findMaximumContiguousSubsum(arr);
+      </code>
+      </pre>
+    </div>
+  </main>
 </template>
