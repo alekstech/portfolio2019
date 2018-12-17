22f049a983b8e097a991159f324eaa516e369813
Aleks Sobieraj <contact@aleks.tech>
2018-12-16 19:35:06 -0500

    vscode settings

---
+++ pages/notes/.vscode/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <pre>
+    <h1>.vscode</h1>
+    {
+      "editor.fontFamily": "'Fira Code', Menlo, Monaco, 'Courier New', monospace",
+      "editor.fontSize": 14,
+      "editor.minimap.enabled": false,
+      "editor.multiCursorModifier": "alt",
+      "editor.renderWhitespace": "boundary",
+      "editor.tabSize": 2,
+      "editor.wordWrap": "on",
+      "eslint.autoFixOnSave": true,
+      "terminal.integrated.fontSize": 14,
+      "terminal.integrated.rendererType": "dom",
+      "terminal.integrated.shell.osx": "/usr/local/bin/zsh",
+      "terminal.integrated.shell.windows": "C:\\WINDOWS\\sysnative\\bash.exe",
+      "tslint.autoFixOnSave": true,
+      "window.restoreWindows": "all",
+      "window.zoomLevel": 1,
+      "workbench.editor.enablePreviewFromQuickOpen": false,
+      "workbench.sideBar.location": "right",
+      "workbench.startupEditor": "none",
+      "workbench.statusBar.feedback.visible": false,
+    }
+  </pre>
+</template>
