d14a67fa40fea019ebb2783282edd85bb6423a50
Aleks Sobieraj <contact@aleks.tech>
2019-01-09 16:19:32 -0500

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,43 @@
 <template>
   <main class="container h-full flex flex-col justify-center items-center bg-yellow-lightest">
-    <p 
-      aria-label="cheshch footnote one text-xl">
-      Cześć <sup><small>*</small></sup>
-    </p>
-    <p class="mt-4 text-lg">My name is Aleks</p>
-    <p class="mt-2 text-lg">I'm a web developer</p>
-    <div class="mt-8 flex items-center">
-      <img 
-        src="~/assets/images/LocationPinIcon.svg" 
-        alt="Location marker"
-        class="fill-current h-4 w-4 mr-2">
-      <p class="text-sm">Toronto</p>
+    <div class="max-w-sm text-center">
+      <p 
+        aria-label="cheshch footnote one"
+        class="text-3xl text-left"
+      >
+        Cześć<sup><small>*</small></sup>,
+      </p>
+      <p class="mt-4 text-xl">My name is Aleks</p>
+      <p class="mt-2 text-xl">I'm a web developer</p>
+      <div class="mt-8 flex justify-center items-center">
+        <img 
+          src="~/assets/images/LocationPinIcon.svg" 
+          alt="Location marker"
+          class="fill-current h-4 w-4 mr-2">
+        <p class="text-base">Toronto</p>
 
+      </div>
+      <p class="mt-12 font-serif leading-normal text-xl text-left text-base">
+        I know how to make an server-side rendered React web app and a GraphQL backend for it. But I couldn't solve an XOr gate problem without a reference manual. Come to think of it, I couldn't do most of my work without reference manuals.
+      </p>
+      <p
+        class="mt-12 font-serif text-left text-base font-light"
+        aria-label="legend"
+      >
+        <small>
+          * Polish; 
+          pron. /'cheshch/;
+          <span class="italic">noun, interjection</span>: 
+          <span>hello </span>
+        </small>
+      </p>
+      <nuxt-link
+        to="/portfolio"
+        tag="button"
+        class="bg-transparent hover:bg-teal-lighter text-base text-black font-lighter hover:text-yellow-lightest mt-8 py-4 px-8 border border-teal-lighter rounded focus:border-pink-dark focus:outline-none"
+      >
+        projects
+      </nuxt-link>
     </div>
-    <p
-      class="mt-6 font-serif text-sm font-light"
-      aria-label="legend"
-    >
-      <small>
-        * Polish; 
-        pron. /'cheshch/;
-        <span class="italic">noun, interjection</span>: 
-        <span>hello </span>
-      </small>
-    </p>
-    <nuxt-link
-      to="/portfolio"
-      tag="button"
-      class="bg-transparent hover:bg-teal-lighter text-xs text-black font-lighter hover:text-yellow-lightest mt-8 py-2 px-8 border border-teal-lighter rounded focus:border-pink-dark focus:outline-none"
-    >
-      projects
-    </nuxt-link>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -249,9 +249,9 @@ module.exports = {
     sm: '.875rem', // 14px
     base: '1rem', // 16px
     lg: '1.125rem', // 18px
-    xl: '1.25rem' // 20px
-    // '2xl': '1.5rem', // 24px
-    // '3xl': '1.875rem', // 30px
+    xl: '1.25rem', // 20px
+    '2xl': '1.5rem', // 24px
+    '3xl': '1.875rem' // 30px
     // '4xl': '2.25rem', // 36px
     // '5xl': '3rem' // 48px
   },
@@ -850,7 +850,7 @@ module.exports = {
     fonts: ['responsive'],
     fontWeights: ['responsive', 'hover', 'focus'],
     height: ['responsive'],
-    leading: false,
+    leading: ['responsive'],
     lists: ['responsive'],
     margin: ['responsive'],
     maxHeight: ['responsive'],
